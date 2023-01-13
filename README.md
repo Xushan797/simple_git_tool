327ea9530d45abc8ec7588c870c3ec454406f63b
Xushan797 <xushanqing797@gmail.com>
2023-01-12 22:09:16 -0800

    update readme and comment

--- README.md
+++ README.md
@@ -1,4 +1,68 @@
-# hello-world
-first test github repository.
+Pronto.ai take home coding
 
-My name is Xushan, this is my new account on github.
+
+> A simple tool that prints specific facts about a local git repository.
+
+
+## Getting Started
+
+Project instructions  
+Your task is to create a program that prints specific facts about a local git repository. You can use any language, and you are welcome to import existing packages.  
+
+Your program should take in one argument:  
+git_dir: directory in which to assess git status  
+  
+Your program should print the following things:  
+active branch (boolean)  
+whether repository files have been modified (boolean)  
+whether the current head commit was authored in the last week (boolean)  
+whether the current head commit was authored by Rufus (boolean)  
+  
+Sample Output Format:  
+The program should print something like:  
+active branch: master  
+local changes: False  
+recent commit: True  
+blame Rufus: True  
+
+### Prerequisites 
+
+The submission can be tested with a MacOS with Python 3. I develop and test the service using a MacOs with Python 3.10.0.
+
+### Setup & Build
+
+Clone this repo and check out to the corresponding repo path. Run the following command:
+
+```sh
+python3 git_func.py  
+```
+
+### Simple test example
+
+I run the code via my local environment:
+
+```sh
+/Users/loaner/Desktop/leetcode/venv/bin/python /Users/loaner/Desktop/leetcode/test/hello-world/git_func.py
+Enter the path of your file: /Users/loaner/Desktop/leetcode/test/hello-world/
+active branch: main
+local changes: True
+recent commit: True
+blame Rufus: False
+```
+
+
+  
+## Implementation
+
+I mainly use Python subprocess package to call the corresponding git command.  
+First, check the input path is exists.  
+Then it would print out the information as needed.  
+  
+(in my understanding:  
+active branch: The branch you are currently working on  
+whether repository files have been modified: whether uncommited changes exists  
+current head commit: last commit)
+
+## Authors 
+
+* **Xushan Qing** - *Duke University* - [Xushan Qing](https://www.linkedin.com/in/xushan-qing/)
--- git_func.py
+++ git_func.py
@@ -1,26 +1,69 @@
-import subprocess
+'''
+Project instructions
+Your task is to create a program that prints specific facts about a local git repository. You can use any language, and you are welcome to import existing packages.
 
+Your program should take in one argument:
+git_dir: directory in which to assess git status
+
+Your program should print the following things:
+active branch (boolean)
+whether repository files have been modified (boolean)
+whether the current head commit was authored in the last week (boolean)
+whether the current head commit was authored by Rufus (boolean)
+
+Sample Output Format
+The program should print something like
+active branch: master
+local changes: False
+recent commit: True
+blame Rufus: True
+'''
+
+import subprocess, os
 
 class GitTool:
-
     def __init__(self):
-        self.author = 'Xushan797'
+        self.author = 'Rufus'
+        self.cwd = None
+    def check_input(self):
+        user_input = input("Enter the path of your file: ")
+        while not os.path.exists(user_input):
+            print("I did not find the file at you input path! " + str(user_input))
+            user_input = input("Enter the path of your file: ")
+        return user_input if user_input else None
     def start(self):
-        print(self.authored())
+        try:
+            self.cwd = self.check_input()
+            branch_name = self.get_active_branch()
+            modified_status = self.modified_status()
+            commited_status = self.commited_last_week()
+            author_status = self.authored()
+            txt = "active branch: {branch_name}\nlocal changes: {modified_status}\nrecent commit: {commited_status}\nblame Rufus: {author_status}\n".format \
+            (branch_name = branch_name, modified_status = modified_status, commited_status = commited_status, author_status = author_status)
+            print(txt)
+        except:
+            print("Sorry, unknown error happened. The environment may be not supported! ")
 
     def get_active_branch(self):
-        output = subprocess.getoutput("git rev-parse --abbrev-ref HEAD")
+        # active branch (boolean)
+        output = subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"]).decode()
+
+        return output.strip()
 
     def modified_status(self):
-        pass
+        # whether repository files have been modified (boolean)
+        output = subprocess.check_output(["git", "diff-index", "--name-only", "HEAD"]).decode()
+        return output is not None
+
+    def commited_last_week(self):
+        # whether the current head commit was authored in the last week (boolean)
+        output = subprocess.check_output(["git", "log", "-1", "--since=\"1 seconds ago\""]).decode()
+        return output is not None
+
     def authored(self):
-        # git log -1 --pretty=format:\'%an\'
-        output = subprocess.getoutput("git log -1 --pretty=format:\'%an\'")
-        # result = subprocess.run(['git', 'log', '-1', '--pretty=format:\'%an\''], stdout=subprocess.PIPE)
-        # author =  result.stdout.decode('utf-8')
+        # whether the current head commit was authored by Rufus (boolean)
+        output = subprocess.check_output(["git", "log", "-1", "--pretty=format:\'%an\'"]).decode()
         return output == self.author
-
-
 
 if __name__ == "__main__":
     gittool = GitTool()
